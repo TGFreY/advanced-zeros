4c97977001b557a0f70fdddf301ba87c80473328
TGFreY <t.g.frey19@gmail.com>
2018-09-18 21:08:47 +0300

    implement zeros counter
    with help of Legendre's formula

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let zeros = number;
+    let tmpBase = base;
+
+    for (let i = 2; i <= base; i++) {
+        if (tmpBase % i === 0) {
+            let tmpNumber               = number;
+            let numberPower             = 0;
+            let primeNumCountInDivide   = 0;
+
+            while (tmpBase % i === 0) {
+                numberPower++;
+                tmpBase = tmpBase / i;
+            }
+
+            while (tmpNumber > 0) {
+                tmpNumber = Math.floor(tmpNumber / i);
+                primeNumCountInDivide += tmpNumber;
+            }
+
+            zeros = Math.floor(Math.min(zeros, primeNumCountInDivide / numberPower));
+        }
+    }
+    return zeros;
 }
